e9a69933d06c46032b1367709941f4e3b1301a44
luis14 <luispacheco14@live.com>
2017-10-21 22:27:37 -0600

    TestCases

--- TestCases/Class1.cs
+++ TestCases/Class1.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using NUnit.Framework;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CRM;
+using CRM.Controllers;
 
 namespace TestCases
 {
-    public class Class1
+    [TestFixture]
+    class TestLogin
     {
+        public void isValidUser()
+        {
+            var Login = Substitute.For<LoginController>();
+
+        }
+        
     }
 }
